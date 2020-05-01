b7a01c90624045ca368a42c3881a420208d573df
agama <a.makarewicz@student.mini.pw.edu.pl>
2020-05-01 23:48:15 +0200

    4 zad 2

--- main.py
+++ main.py
@@ -179,3 +179,14 @@ async def display_tracks(page: int = 0, per_page: int = 10):
 		f"SELECT * FROM tracks LIMIT {per_page} OFFSET {page*per_page}"
 		).fetchall()
 	return tracks
+
+
+@app.get("/tracks/composers")
+async def display_titles(composer_name: str):
+	app.db_connection.row_factory = lambda cursor, row : row[0]
+	tracks = app.db_connection.execute(
+		"SELECT name FROM tracks WHERE composer = ? ORDER BY name",
+		(composer_name,)).fetchall()
+	if len(tracks) <= 0:
+		raise HTTPException(status_code=404, detail={"error": "Item not found"})
+	return tracks
